d058377734945d307e37bb1ef0b000a2e76da35c
Adrian Piwowarczyk <adrpiw@gmail.com>
2018-03-27 21:24:58 +0000

    Handling POST requests data processing

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const request = require('request');
 const sharp = require('sharp');
+const multer = require('multer');
+const upload = multer();
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.get('/resize', function (httpRequest, httpResponse) {
     const {url, width = 1920, height = 1080, format} = httpRequest.query;
-    const resizedImage = resize(width, height, format);
+    const resizedImage = resizeStream(width, height, format);
 
     const inputType = url.match('.png|.jpg|.jpeg')[0];
     const outputType = inputType.split('.')[1];
 
-    httpResponse.set(`Content-Type', 'image/${outputType}`);
+    httpResponse.set('Content-Type', `image/${outputType}`);
 
     request
         .get(url)
@@ -23,9 +31,38 @@ app.get('/resize', function (httpRequest, httpResponse) {
 
 });
 
-function resize(width, height, format) {
+app.post('/resize', upload.single('url'), function (httpRequest, httpResponse) {
+    const {file} = httpRequest;
+    const {buffer} = file;
+    const {width = 1920, height = 1080, format} = httpRequest.body;
+
+    const inputType = file.originalname.match('.png|.jpg|.jpeg')[0];
+    const outputType = inputType.split('.')[1];
+
+    httpResponse.set('Content-Type', `image/${outputType}`);
+
+    const resizedImage = resizeBufferPromise(buffer, width, height, format);
+
+    resizedImage.then((buffer) => {
+        httpResponse.end(buffer);
+    });
+
+});
+
+function resizeStream(width, height, format) {
     let output = sharp()
         .resize(parseInt(width), parseInt(height));
+
+    if (format === 'jpeg') output.jpeg();
+    if (format === 'png') output.png();
+
+    return output;
+}
+
+function resizeBufferPromise(buffer, width, height, format) {
+    let output = sharp(buffer)
+        .resize(parseInt(width), parseInt(height))
+        .toBuffer();
 
     if (format === 'jpeg') output.jpeg();
     if (format === 'png') output.png();
